5f2accfd105c6d13707b4d9be77915dea6f65ae6
DimonAgon <80159089+DimonAgon@users.noreply.github.com>
2023-06-05 14:01:19 -0700

    add basic templates

--- May_green_again/urls.py
+++ May_green_again/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from shop.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('gardensite/', gardensite, name='gardensite'),
+    path('cart/', cart, name='cart')
 ]
