c37110f1cfe4842e54178cc216586ebfd7916a1d
DimonAgon <dimon2003agon@gmail.com>
2023-11-07 16:05:08 +0200

    add tag to admin

--- shop/admin.py
+++ shop/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Good, Plant,  Cart, ordered_good
+from .models import Good, Tag, Plant, Cart, ordered_good
 
-for model in (Good, Cart, Plant, ordered_good):
+for model in (Good, Tag, Cart, Plant, ordered_good):
     admin.site.register(model)
