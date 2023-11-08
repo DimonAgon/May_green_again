0e31d8ff2d02b0e14f96f1e92241728837dc3396
DimonAgon <dimon2003agon@gmail.com>
2023-11-09 01:28:40 +0200

    add media url

--- May_green_again/urls.py
+++ May_green_again/urls.py
@@ -14,11 +14,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from shop.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('gardensite/', gardensite, name='gardensite'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
